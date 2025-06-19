1a20a21183c51155ad51425a01da81046d8d58a5
Franchette Camoro <nicolecamoro@gmail.com>
2025-06-19 20:12:20 +0800

    feat: update skills description

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -3,34 +3,42 @@
 import Image from "next/image";
 import SkillsHeader from "@/app/assets/images/skills-header.svg";
 import Tabs from "./atoms/Tabs";
-import skillsData from "@/app/data/skills.json";
+import skillsData from "@/app/data/skills.json"; // Fixed import path
 import { useState } from "react";
 
 type Category = "Frontend" | "Backend" | "Practices" | "Tools";
+type Skill = {
+  name: string;
+  description: string;
+  logo: string;
+};
 
 export default function Skills() {
   const [selectedTab, setSelectedTab] = useState(0);
   const categories: Category[] = ["Frontend", "Backend", "Practices", "Tools"];
-  const currentSkills = skillsData.skills[categories[selectedTab]];
+  const currentSkills = skillsData.skills[categories[selectedTab]] as Skill[];
 
   return (
     <section id="skills" className="min-h-screen bg-peach py-20 relative">
       <div className="w-full h-full">
-        {/* Skills Grid */}
         <div className="flex items-center justify-center min-h-screen mx-auto">
-          <div className="flex flex-col">
+          <div className="flex flex-col w-screen md:w-9/12">
             <Tabs selectedTab={selectedTab} onTabChange={setSelectedTab} />
-            <div className="grid grid-cols-1 gap-8 mb-20 md:grid-cols-2 lg:grid-cols-4 min-w-6xl bg-beige p-4 rounded-sm">
-              {currentSkills.map((skill: string) => (
-                <div
-                  key={skill}
-                  className="flex items-center justify-center transition-shadow bg-white border-2 rounded-lg aspect-square border-beige hover:shadow-lg scale-none hover:scale-120 transition-all duration-500"
-                >
-                  <div className="text-center text-gray-500 font-inter">
-                    <div className="text-sm">{skill}</div>
+            <div className=" bg-beige p-8 rounded-sm">
+              <div className="flex flex-wrap gap-6 justify-center">
+                {currentSkills.map((skill) => (
+                  <div
+                    key={skill.name}
+                    className="w-[250px] text-start text-white/90 font-inter tracking-wider py-6 px-4 bg-navy rounded-lg shadow-sm"
+                  >
+                    {/* <i className={`${skill.logo} text-2xl mb-3 block`}></i> */}
+                    <div className="text-xl font-bold italic mb-2">
+                      {skill.name}
+                    </div>
+                    <p className="text-sm ">{skill.description}</p>
                   </div>
-                </div>
-              ))}
+                ))}
+              </div>
             </div>
           </div>
         </div>
